db7c8dd43d5db14b5cca2e73818942a7787c6712
cool-library-2371 <est.ariel@gmail.com>
2020-03-12 18:32:03 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,10 +4,11 @@ var height = "74"
 
 if (name === "Susan") {
     console.log("The name is correct")
-} 
-
-else {
+}  else {
     console.log("Expected the name to be Susan")
+
+
+
 // Don't worry about this
 module.exports = { name, height
 }
